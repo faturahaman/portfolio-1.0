10b5f39e2fd2438d6cf9120819d33adea45582e5
faturahman <faturmriffa@gmail.com>
2026-06-06 10:31:40 +0700

    robot.ts allowing meta data routes

--- app/robots.ts
+++ app/robots.ts
@@ -3,8 +3,9 @@ import { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: "*",
-      allow: "/",
+      userAgent: '*',
+      allow: '/',
+      disallow: ['/admin/', '/api/', '/private/'],
     },
     sitemap: "https://riffatur.com/sitemap.xml",
   };
